e4b14c2bb0f333a0348c97352ffec1f49e39f94e
Zenkiez22 <96461012+Zenkiez22@users.noreply.github.com>
2023-05-11 12:08:31 +0800

    add dependency

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,24 @@
 const express = require ("express")
+const dotenv = require("dotenv")
+const { chats } = require("./data/data.js");
 
-const app = express()
+const app = express();
+dotenv.config();
 
-app.listen(5000,console.log("Server Started on PORT 5000"));
+app.get('/', (req, res) => {
+    res.send("API is Running Successfully");
+});
+
+app.get('/api/chat',(req, res) => {
+    res.send(chats);
+})
+
+app.get('/api/chat/:id',(req, res)=>{
+    // console.log(req.params.id);
+    const singleChat = chats.find((c) => c._id ===req.params.id);
+    res.send(singleChat);
+    console.log(singleChat);
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(5000,console.log(`Server Started on PORT ${PORT}`));
